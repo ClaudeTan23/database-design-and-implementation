07033186536aecec3e1912c88c1051388b243767
Claude Tan <88834398+ClaudeTan23@users.noreply.github.com>
2024-03-10 17:06:12 +0800

    Update databaseRestore.bat

--- databaseRestore.bat
+++ databaseRestore.bat
@@ -11,3 +11,5 @@ set databaseSql=community_portal-05-03-2024-21-36-50-81.sql
 if [%password%]==[] (mysql -h %sqlIp% -u %username% %database% < %cd%\%dbBackupfolder%\%databaseSql%) else (mysql -h %sqlIp% -u %username% -p %password% %database% < %cd%\%dbBackupfolder%\%databaseSql%)
 
 if %ERRORLEVEL% NEQ 0 ( echo Database restore failed ) else ( echo Database restore successfully )
+
+SET /p key=Type any key to exit...
